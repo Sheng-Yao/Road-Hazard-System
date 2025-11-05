469804ce3c74b19841b3e320dc846d078db8307a
Sheng-Yao <hengshengyao2002@gmail.com>
2025-11-05 14:22:00 +0800

    Changed app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
 import psycopg2, os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(os.environ["DATABASE_URL"])
+def get_conn():
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
 @app.route("/stats")
 def get_stats():
+    conn = get_conn()
     cur = conn.cursor()
     cur.execute("SELECT name, value FROM stats;")
     rows = cur.fetchall()
+    conn.close()
     return jsonify([{"name": r[0], "value": r[1]} for r in rows])
